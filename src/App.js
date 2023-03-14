5a49a88b8228e2d64c346b9e3cee96301613aedc
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-03-14 23:49:23 +0800

    updated the book list object, and rendered the list in the List component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,48 @@ import Search from "./components/Search";
 import List from "./components/List";
 
 function App() {
+  const items = [
+    {
+      id: 1,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+    },
+    {
+      id: 2,
+      title: "Harry Potter and the Order of Phoenix",
+      author: " J.K. Rowling",
+    },
+    {
+      id: 3,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+    },
+    {
+      id: 4,
+      title: "The Diary of a Young Girl",
+      author: "Anne Frank",
+    },
+    {
+      id: 5,
+      title: "Animal Farm",
+      author: "George Orwell",
+    },
+    {
+      id: 6,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+    },
+    {
+      id: 7,
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+    },
+  ];
+
   return (
     <>
       <Search />
-      <List />
+      <List list={items} />
     </>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,3 +1,11 @@
-function List() {}
+function List({ list }) {
+  return (
+    <ul>
+      {list.map((book) => {
+        return <li key={book.id}>{book.title}</li>;
+      })}
+    </ul>
+  );
+}
 
 export default List;
